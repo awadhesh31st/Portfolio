8f653d9c1ec00e532212745adc53abe96ad90f08
awadhesh31st <awadhesh31st@gmail.com>
2023-05-09 20:46:02 +0530

    updated contect page

--- mocks/navigation-mock.ts
+++ mocks/navigation-mock.ts
@@ -2,7 +2,8 @@ import { NavigationsProps } from '../types/navigation';
 
 export const NavigationMock: NavigationsProps = {
   navigations: [
-    { title: 'Projects', path: '/project' },
-    { title: 'Contact', path: '/contact' },
+    { title: 'projects', path: '/project' },
+    { title: 'about', path: '/about' },
+    { title: 'contact', path: '/contact' },
   ],
 };
--- types/contect.d.ts
+++ types/contect.d.ts
@@ -4,4 +4,7 @@ export type ContectProps = {
   navigationsProps: NavigationsProps;
   pageTitle: string;
   pageDescription: string;
+  placeHolderText: string;
+  buttonLabel: string;
+  linkedin: string;
 };
