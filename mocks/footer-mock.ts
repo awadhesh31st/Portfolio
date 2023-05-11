3e2c367459fc9f0818b21b986dfdda422dfb7e20
awadhesh31st <awadhesh31st@gmail.com>
2023-05-11 13:08:01 +0530

    page not found and update footer

--- mocks/footer-mock.ts
+++ mocks/footer-mock.ts
@@ -3,14 +3,14 @@ import { FooterProps } from '../types/footer';
 export const FooterMock: FooterProps = {
   stuff: {
     title: 'Stuff',
-    list: ['Project', 'Contact'],
+    list: ['Project', 'Contact', 'About'],
   },
   moreStuff: {
-    title: 'More Stuff',
+    title: 'More',
     list: ['Blog'],
   },
   formTitle: 'Connect with me',
-  formDescription: 'Stay up to date on the latest React news.',
+  formDescription: 'Share your email for the latest React news.',
   placeholder: 'Enter your email',
   btnLabel: 'Submit',
 };
