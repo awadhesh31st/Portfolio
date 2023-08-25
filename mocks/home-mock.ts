835925f2e1fdeaa1417a1866e5d4a33ac6bd87ca
Awadhesh Kumar <91739540+awadhesh31st@users.noreply.github.com>
2023-08-25 15:31:19 +0530

    Update home-mock.ts
    Update home about content

--- mocks/home-mock.ts
+++ mocks/home-mock.ts
@@ -4,5 +4,5 @@ import { LayoutProps } from '../types/common';
 export const HomeMock: LayoutProps = {
   navigationsProps: NavigationMock,
   title: `hi, i'm awadhesh`,
-  description: `As a software developer with 3 years of experience in the IT industry, my passion lies in front-end development. I have created courses on React, Next.js, and web development to share my knowledge with others. I am constantly learning and adapting to new technologies to deliver high-quality solutions.`,
+  description: `As a software developer with 4+ years of experience in the IT industry, my passion lies in front-end development. I have created courses on React, Next.js, and web development to share my knowledge with others. I am constantly learning and adapting to new technologies to deliver high-quality solutions.`,
 };
